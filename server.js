77e6a845804a44f9496fa8e86270ccfd34b9ea0f
tomlinovic <150364007+tomlinovic@users.noreply.github.com>
2026-04-07 09:18:27 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    const dataPath = path.join(__dirname, 'images.json');
-    const images = JSON.parse(fs.readFileSync(dataPath));
-    res.render('slike', { images });
+res.send('Pozdrav sa Railway servera!');
 });
-
-app.listen(3000);
+app.listen(PORT, () => {
+console.log('Server pokrenut na portu ${PORT}');
+app.use(express.static('public'));
+});
